652fe10d9791ad34895e04d3e81f3f61471d2cd7
Aymen Kmar <aymen.kmar@satoripop.com>
2025-05-28 23:43:43 +0000

    change pagination in node to 10

--- resources/views/livewire/kubernetes/node-list.blade.php
+++ resources/views/livewire/kubernetes/node-list.blade.php
@@ -241,7 +241,7 @@
 
                 // Pagination
                 currentPage: 1,
-                perPage: 25,
+                perPage: 10,
 
                 init() {
                     this.filterNodes();
